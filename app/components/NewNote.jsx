49a011854d4cdbb036121ee319418c862d00b896
xiaomashop <xiaoma.shopify@gmail.com>
2023-03-18 18:08:56 +0800

    form submitting state, form validation

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,20 +1,37 @@
+import {
+  Form, 
+  useNavigation, 
+  useActionData }
+  from '@remix-run/react';
 import newNoteStyles from '~/components/NewNote.css';
 
 export default function NewNote() {
+  
+  // access the return of action
+  const data = useActionData();
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  console.log("navigation.state: ", navigation.state )
+  console.log("isSubmitting: ", isSubmitting);
+
   return (
-    <form method="post" id="note-form" action="/notes">
+    <Form method="post" id="note-form" action="/notes">
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
+        {data?.message && <p>{data.message}</p>}
       </p>
       <p>
         <label htmlFor="content">Content</label>
         <textarea id="content" name="content" rows="5" required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Adding" : "Add Note"}
+        </button>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -5,6 +5,7 @@ import NoteList, {links as noteListLinks} from "../components/NoteList";
 import { getStoredNotes, storeNotes } from "../data/note";
 
 export default function NotesPage(){
+    // access the return of loader
     const notes = useLoaderData();
     return <main>
         <NewNote />
@@ -27,12 +28,22 @@ export async function loader() {
 export async function action({ request }){
     const formData = await request.formData();
     const noteData = Object.fromEntries(formData);
+    if(noteData.title.trim().length<5){
+        return {message: 'Invalid title - must be at least 5 characters long.'}
+    }
     noteData.id = new Date().toISOString();
     //console.log(noteData);
     const existingNotes = await getStoredNotes();
     const updateNotes = existingNotes.concat(noteData);
     await storeNotes(updateNotes);
+    await new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve(),2000)
+    });
     // return a response to redirect the user
+    /*
+    If we use <Form > in NewNote component, redirect will do client side routing
+    instead of triggering a new request that would actually fetch a new page in the case we use <form >
+    */
     return redirect("/notes");
 }
 //surfacing link
